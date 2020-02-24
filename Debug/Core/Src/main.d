924370f2cef3376326ef241ed6fd0197f40f9725
autraurdo <autraurdo@gmail.com>
2020-02-24 16:13:48 +0200

    use button

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -1,28 +1,28 @@
 Core/Src/main.o: ../Core/Src/main.c \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/stm32f3xx.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/stm32f303xc.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/core_cm4.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_version.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_compiler.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_gcc.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/mpu_armv7.h \
- D:/STUDENTS/startCmsisF303/CMSIS/Inc/system_stm32f3xx.h \
- D:/STUDENTS/startCmsisF303/Core/Inc/main.h
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/stm32f3xx.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/stm32f303xc.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/core_cm4.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_version.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_compiler.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_gcc.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/mpu_armv7.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/system_stm32f3xx.h \
+ D:/STUDENTS/startCmsisF303/learnCMSIS/Core/Inc/main.h
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/stm32f3xx.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/stm32f3xx.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/stm32f303xc.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/stm32f303xc.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/core_cm4.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/core_cm4.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_version.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_version.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_compiler.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_compiler.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/cmsis_gcc.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/cmsis_gcc.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/mpu_armv7.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/mpu_armv7.h:
 
-D:/STUDENTS/startCmsisF303/CMSIS/Inc/system_stm32f3xx.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc/system_stm32f3xx.h:
 
-D:/STUDENTS/startCmsisF303/Core/Inc/main.h:
+D:/STUDENTS/startCmsisF303/learnCMSIS/Core/Inc/main.h:
--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -15,5 +15,5 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Src/main.o: ../Core/Src/main.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32F303VCTx -DSTM32 -DSTM32F3 -DDEBUG -c -I"D:/STUDENTS/startCmsisF303/CMSIS/Inc" -I"D:/STUDENTS/startCmsisF303/Core/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32F303VCTx -DSTM32 -DSTM32F3 -DDEBUG -c -I"D:/STUDENTS/startCmsisF303/learnCMSIS/CMSIS/Inc" -I"D:/STUDENTS/startCmsisF303/learnCMSIS/Core/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
